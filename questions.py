b26d3edd5ede2b68a817d89eb38d0196f5afbbd0
Alex Chen <60804664+alexchen2@users.noreply.github.com>
2023-01-15 02:47:54 -0500

    Restructure Files + Add video r/w functionality

---
+++ questions.py
@@ -0,0 +1,187 @@
+"""
+questions.py - reads from and writes to JSON file of interview questions for later use
+(Alex Chen)
+"""
+
+# Import packages
+import json
+import random as rd
+
+def readFile(filename : str = "data/questions.json"):
+    """
+    Function that reads a JSON file and returns its contents (questions.json by default).
+    :param filename: name of the custom JSON file being read, if any. Otherwise, defaults to "questions.json".
+    """
+    # Read JSON file
+    try:
+        with open(filename, 'r') as fh:
+            reader = fh.read()
+            database = json.loads(reader)
+    except FileNotFoundError:
+        # Temporary, for debugging/testing
+        print("Error at 22, questions.py: the file questions.json has been moved. Please put the file back in its original location in \
+               the data folder.")
+        assert 1 == 0
+
+    return database
+
+def writeFile(contents : dict, filename : str = "data/questions.json"):
+    """
+    Function that writes a dictionary to a JSON file (questions.json by default).
+    :param contents: dictionary representing data that is being written to the JSON.
+    :param filename: name of the custom JSON file being read, if any. Otherwise, defaults to "questions.json".
+    """
+    with open(filename, "w") as fh2:
+        database = json.dumps(contents)
+        fh2.write(database) 
+
+
+def readQuestion(chooseQID : int = None):
+    """
+    Reads either a random question from a JSON file of questions, or if qID is given, then returns a specific question from 
+    the database.
+    :param chooseQID: ID assigned to each question in the JSON; increments from 1, and should be an integer.
+    """
+    question = ""
+    content = ""
+    qIDValid = True
+
+    database = readFile()
+    storedQID = [num for num in database if not database[num]["hasSeen"]] # List of all question IDs in JSON
+    
+    # Get question content from JSON file
+    while True:
+        # If list of question IDs to randomly choose from is empty, reset hasSeen status to false for all questions
+        if storedQID == []:
+            print("Debug: No random question available; resetting hasSeen status for all questions")
+            resetReadState()
+            database = readFile()
+            storedQID = [num for num in database if not database[num]["hasSeen"]]
+
+        # If qID not given or is invalid, then generates a random number to pick out a random question from JSON
+        if chooseQID == None or not qIDValid:
+            qID = str(rd.choice(storedQID))
+        else:
+            qID = str(chooseQID)
+
+        try:
+            question = database[qID]
+            
+            # If not requesting a new question:
+            if chooseQID != None:
+                content = question["content"]
+                break
+            else:     # Otherwise, if randomly choosing a new question:
+                # If question has already been seen before, delete qID entry from storedQID and pick another random Q
+                if question["hasSeen"]:
+                    del(storedQID[qID])       
+                    continue
+                else: 
+                    content = question["content"]
+                    database[qID]["hasSeen"] = True  
+                    break                  
+
+        except KeyError:
+            print("Question ID not found, choosing random question instead") # temp for testing
+            qIDValid = False
+            continue
+    
+    # Modify and write JSON
+    writeFile(database)
+    print("Read Q Test Success")     
+    return {qID : content}
+
+def addQuestion(qContent : str):
+    """
+    (Optional feature) Adds a question from the user into the database.
+    :param qContent: String containing the question itself.
+    """
+    database = readFile()
+    newQID = len(database) + 1
+    question = {
+        "content": qContent,
+        "hasSeen": False
+    }
+    
+    database[newQID] = question
+    writeFile(database)
+
+
+def resetReadState():
+    """
+    Resets hasSeen setting for every question in questions.json.
+    """
+    database = readFile()
+
+    for question in database.values():
+        question["hasSeen"] = False
+
+    writeFile(database)  
+
+
+def setVideoPath(qID : int, filePath : str):
+    """
+    Sets the video path of the question with the given filePath to the specified question ID from the JSON file.
+    :param qID: ID assigned to each question in the JSON; increments from 1, and should be an integer.
+    :param filePath: String of the filePath to store in the JSON.
+    """
+    database = readFile()
+    qID = str(qID)
+
+    try:
+        database[qID]["videoPath"] = filePath
+    except KeyError:
+        print(f"Debug: Question ID #{qID} does not exist.")
+        assert 1 == 0
+    
+    writeFile(database)
+    print("Set Video Path Test Success")   # remove later
+
+
+def readVideoPath(qID : int):
+    """
+    Gets the video path of the question with the specified question ID from the JSON file, if it exists.
+    """
+    database = readFile()
+    qID = str(qID)
+
+    try:
+        output = database[qID]["videoPath"]
+    except KeyError:
+        print(f"Debug: Question ID #{qID} does not exist.")
+        assert 1 == 0
+
+    return output    
+
+
+def readSeenQuestions():
+    """
+    Returns a dictionary of questions that have been viewed before in the format {<questionID>: <question content>}.
+    """
+    database = readFile()
+    output = {qID : database[qID]["content"] for qID in database if database[qID]["hasSeen"]}
+    
+    return output
+
+
+def main():
+    """
+    Just here for debugging.
+    """
+    # setVideoPath(4, "test/path/video.mp4")
+    # test = readVideoPath(4)
+    # print(test)
+    resetReadState()
+
+    # test = readSeenQuestions()
+    # for i in test:
+    #     print(f"#{i}: {test[i]}")
+
+    # addQuestion("What is the meaning of life?")
+
+    test = readQuestion()
+    print(test)
+
+if __name__ == "__main__":
+    main()
+
---
+++ videos.py
@@ -0,0 +1,140 @@
+"""
+videos.py - reads from and writes to JSON file of interview questions for later use
+(Alex Chen)
+"""
+# Import modules
+import json
+import os
+from questions import readFile, writeFile
+
+# Custom exception for the case where reading a QID from videos.json yields no info
+class QIDNotFoundError(Exception):
+    pass
+
+def binVideoSearch(videos, firstIndex, lastIndex, qID):
+    """
+    Binary search algorithm used for searching through videos.json for 
+    """
+    if firstIndex <= lastIndex:
+
+        midIndex = (lastIndex + firstIndex) // 2
+        check = videos[midIndex]["QID"]
+
+        if check == qID:
+            return midIndex
+            # return videos[midIndex]          # An alternative; see how JSON dicts are parsed in Javascript
+        elif check > qID:
+            return binVideoSearch(videos, firstIndex, midIndex - 1, qID)
+        else:
+            return binVideoSearch(videos, midIndex + 1, lastIndex, qID)
+    else:
+        print("Debug: question ID not found in videos.json; try again with another question ID.")
+        raise QIDNotFoundError
+
+
+def readVideo(qID: int, firstName : str, lastName : str):
+    """
+    Reads either a random question from a JSON file of questions, or if qID is given, then returns a specific question from 
+    the database.
+    :param qID: ID assigned to each video entry in the JSON; increments from 1, and should be an integer.
+    """
+    firstName = firstName.title()
+    lastName = lastName.title()
+    database = readFile(f"data/{firstName}{lastName}/videos.json")
+
+    if database == {}:
+        print("Debug: Nothing in file.")
+        output = [None, None, None, None]
+        return output
+
+    videos = database["Entries"]
+
+    try:
+        index = binVideoSearch(videos, 0, len(videos) - 1, qID)
+        output = [info for info in videos[index].values()]
+    except QIDNotFoundError:
+        output = [None, None, None, None]
+        assert 1 == 0
+
+    return output
+
+
+def writeVideo(filename: str, qID: int, score: int, transcript: str, firstName : str, lastName : str):
+    """
+    Placeholder description
+    """
+    firstName = firstName.title()
+    lastName = lastName.title()
+    database = readFile(f"data/{firstName}{lastName}/videos.json")
+
+    # Increment number of videos, then add info as a dict into "Entries" key
+    videos = database["Entries"]
+    info = {
+        "Filepath": filename,
+        "QID": qID,
+        "Score": score,
+        "Transcript": transcript
+    }
+
+    # Test to see if QID already exists within JSON; if so, replace with new video info, and otherwise, overwrite video entry
+    try:
+        index = binVideoSearch(videos, 0, len(videos) - 1, qID)
+        videos[index] = info
+    except QIDNotFoundError:
+        videos.append(info)
+        print(videos)
+        videos.sort(key=(lambda video : video["QID"])) 
+
+    writeFile(database, f"data/{firstName}{lastName}/videos.json")
+    print("Write Video Test Success")
+
+
+def createVidDir(firstName : str, lastName : str): 
+    """
+    Creates directory to store the video files and video.json in the data folder in root.
+    """
+    firstName = firstName.title()
+    lastName = lastName.title()
+    directory = f"data/{firstName}{lastName}"
+    
+    try:
+        absPath = os.path.dirname(__file__)
+        JSONPath = os.path.join(absPath, directory)
+        os.mkdir(JSONPath)
+    except FileExistsError:
+        print("Debug: Directory already exists")
+        return
+
+    writeFile({"Entries": []}, f"{directory}/videos.json")
+    print("Create dir test successful")
+
+
+def deleteVideo(qID : int, firstName : str, lastName : str):
+    """
+    Placeholder
+    """
+    firstName = firstName.title()
+    lastName = lastName.title()    
+    database = readFile(f"data/{firstName}{lastName}")
+    videos = database["Entries"]
+
+    try:
+        index = binVideoSearch(videos, 0, len(videos) - 1, qID)
+        del(videos[index])
+    except QIDNotFoundError:   # QID doesn't exist, so therefore there's nothing to delete
+        print("Debug: no video to delete")
+        pass
+
+    print("Delete video success")
+    return    
+
+
+def main():
+    createVidDir("Hello", "wOrld")
+    writeVideo("video.wav", 1, 100, "California potato powers", "Hello", "World")
+    test = readVideo(1, "Hello", "World")
+    print(test)
+
+
+if __name__ == "__main__":
+    main()
