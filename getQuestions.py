acf252a439d74870522a6eb6f6c7b7b2ae31feb6
Alex Chen <60804664+alexchen2@users.noreply.github.com>
2023-01-14 22:39:15 -0500

    Update getQuestions.py & questions.json formatting
    - reorganized and optimized some things
    - added functionality for reading and setting video paths
    - reworked getting random questions in readQuestion() function

--- getQuestions.py
+++ getQuestions.py
@@ -10,9 +10,9 @@ import json
 import random as rd
 from bs4 import BeautifulSoup
 
-# Exception for if there are no more unseen questions available to choose from
-class NoRandQuestionLeft(Exception):
-    pass
+# Exception for if there are no more unseen questions available to choose from     # NOT USED
+# class NoRandQuestionLeft(Exception):
+#     pass
 
 
 def readFile(filename : str = "questions.json"):
@@ -33,6 +33,16 @@ def readFile(filename : str = "questions.json"):
 
     return database
 
+def writeFile(contents : dict, filename : str = "questions.json"):
+    """
+    Function that writes a dictionary to a JSON file (questions.json by default).
+    :param contents: dictionary representing data that is being written to the JSON.
+    :param filename: name of the custom JSON file being read, if any. Otherwise, defaults to "questions.json".
+    """
+    with open(filename, "w") as fh2:
+        database = json.dumps(contents)
+        fh2.write(database) 
+
 
 def readQuestion(chooseQID : int = None):
     """
@@ -45,10 +55,17 @@ def readQuestion(chooseQID : int = None):
     qIDValid = True
 
     database = readFile()
-    storedQID = [num for num in database] # List of all question IDs in JSON
+    storedQID = [num for num in database if not database[num]["hasSeen"]] # List of all question IDs in JSON
     
     # Get question content from JSON file
     while True:
+        # If list of question IDs to randomly choose from is empty, reset hasSeen status to false for all questions
+        if storedQID == []:
+            print("Debug: No random question available; resetting hasSeen status for all questions")
+            resetReadState()
+            database = readFile()
+            storedQID = [num for num in database if not database[num]["hasSeen"]]
+
         # If qID not given or is invalid, then generates a random number to pick out a random question from JSON
         if chooseQID == None or not qIDValid:
             qID = str(rd.choice(storedQID))
@@ -56,11 +73,6 @@ def readQuestion(chooseQID : int = None):
             qID = str(chooseQID)
 
         try:
-            # If list of question IDs to randomly choose from is empty, raise exception
-            if storedQID == []:
-                print("Debug: No random question available; pass a specific QID into the function readQuestion()")
-                raise NoRandQuestionLeft
-
             question = database[qID]
             
             # If not requesting a new question:
@@ -83,12 +95,9 @@ def readQuestion(chooseQID : int = None):
             continue
     
     # Modify and write JSON
-    with open("questions.json", "w") as fh2:
-        database = json.dumps(database)
-        fh2.write(database)
-
+    writeFile(database)
     print("Read Q Test Success")     
-    return content
+    return {qID : content}
 
 def addQuestion(qStr : str):
     """
@@ -103,10 +112,7 @@ def addQuestion(qStr : str):
     }
     
     database[newQID] = question
-
-    with open("questions.json", "w") as fh2:
-        database = json.dumps(database)
-        fh2.write(database)    
+    writeFile(database)
 
 
 def resetReadState():
@@ -118,9 +124,42 @@ def resetReadState():
     for question in database.values():
         question["hasSeen"] = False
 
-    with open("questions.json", "w") as fh2:
-        database = json.dumps(database)
-        fh2.write(database)   
+    writeFile(database)  
+
+
+def setVideoPath(qID : int, filePath : str):
+    """
+    Sets the video path of the question with the given filePath to the specified question ID from the JSON file.
+    :param qID: ID assigned to each question in the JSON; increments from 1, and should be an integer.
+    :param filePath: String of the filePath to store in the JSON.
+    """
+    database = readFile()
+    qID = str(qID)
+
+    try:
+        database[qID]["videoPath"] = filePath
+    except KeyError:
+        print(f"Debug: Question ID #{qID} does not exist.")
+        assert 1 == 0
+    
+    writeFile(database)
+    print("Set Video Path Test Success")   # remove later
+
+
+def readVideoPath(qID : int):
+    """
+    Gets the video path of the question with the specified question ID from the JSON file, if it exists.
+    """
+    database = readFile()
+    qID = str(qID)
+
+    try:
+        output = database[qID]["videoPath"]
+    except KeyError:
+        print(f"Debug: Question ID #{qID} does not exist.")
+        assert 1 == 0
+
+    return output    
 
 
 def readSeenQuestions():
@@ -137,15 +176,19 @@ def main():
     """
     Just here for debugging.
     """
-    test = readSeenQuestions()
-    for i in test:
-        print(f"#{i}: {test[i]}")
+    setVideoPath(4, "test/path/video.mp4")
+    test = readVideoPath(4)
+    print(test)
+    # resetReadState()
+
+    # test = readSeenQuestions()
+    # for i in test:
+    #     print(f"#{i}: {test[i]}")
 
     # addQuestion("What is the meaning of life?")
 
-    # test = readQuestion()
-    # print(test)
-
+    test = readQuestion()
+    print(test)
 
 
 if __name__ == "__main__":
